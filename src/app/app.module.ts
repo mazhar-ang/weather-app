792e001bd94645a448ec2612eaf83f9ce04ce1b6
mazhar <mazhar.xoomplus@gmail.com>
2024-11-27 11:27:01 +0500

    Added routing and components for Home and Location

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './pages/home/home.component';
+import { LocationComponent } from './pages/location/location.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LocationComponent
   ],
   imports: [
     BrowserModule,
